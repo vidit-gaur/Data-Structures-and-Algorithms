be9eb2c01f7002d4f4d20818004acc31d436e6bc
vidit <viditgaur1209@gmail.com>
2021-06-20 19:42:00 +0530

    Recursion

---
+++ Recursion/towerOfHanoi.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void TOH(int n,char A,char B,char C){
+    if(n==1){
+        cout<<"Move 1 from"<<A<<" to "<<C<<endl;
+    }
+    TOH(n-1,A,C,B);
+    cout<<"Move"<<n<<"from"<<A<<"to"<<C<<endl;
+    TOH(n-1,B,A,C);
+}
+
+int main(){
+
+    TOH(5,'A','B','C');
+}
