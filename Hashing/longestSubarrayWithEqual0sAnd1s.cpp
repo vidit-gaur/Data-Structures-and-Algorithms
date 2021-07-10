2e87ccc8589aed493195945284c812bd63ba79b9
vidit <viditgaur1209@gmail.com>
2021-07-10 19:33:19 +0530

    Hashing

---
+++ Hashing/longestSubarrayWithEqual0sAnd1s.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+//we will replace all 0s with -1 and make this largestSubarray with sum=0 problem.
+int largestSubarray(bool arr[],int n){
+
+
+}
+
+
+int main(){
+    bool arr[]={0,0,1,1,1,1,1,0};
+    cout<<largestSubarray(arr,8);
+}
