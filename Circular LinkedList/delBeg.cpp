2738e9b93b8400204b53a07184da6ec61de74453
vidit <viditgaur1209@gmail.com>
2021-07-23 19:09:13 +0530

    Circular Linked List

---
+++ Circular LinkedList/delBeg.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node{
+    int data;
+    node *next=NULL;
+    node(int value){
+        data=value;
+        next=NULL;
+    }
+};
+
+void printList(node *head){
+    if(head==NULL) return;
+    cout<<head->data<<" ";
+    for(node *p=head->next;p!=head; p=p->next){
+        cout<<p->data<<" ";
+    }
+}
+
+node *insEnd(node *head,int x){
+    node *temp=new node(x);
+    if(head==NULL){
+        temp->next=temp;
+        return temp;
+    }
+    else{
+        temp->next=head->next;
+        head->next=temp;
+        int t= temp->data;
+        temp->data=head->data;
+        head->data=t;
+        return temp;
+    }
+
+}
+
+node *delBeg(node *head){
+    if(head==NULL) return 0;
+    if(head->next==head){
+        delete head;
+        return 0;
+    } 
+    head->data=head->next->data;
+    node *temp=head->next;
+    head->next=head->next->next;
+    delete temp;
+    return head;
+}
+
+int main(){
+   node *head=NULL;
+    head = insEnd(head, 10) ;
+    head = insEnd(head, 20) ;
+    head = insEnd(head, 30) ;
+    head = insEnd(head, 40) ;
+    head = insEnd(head, 50) ;
+    head=delBeg(head);
+    printList(head);
+
+}
