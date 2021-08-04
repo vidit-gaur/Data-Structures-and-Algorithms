ce48ed716778c949c6a7455917b384333b3d8a5a
vidit <viditgaur1209@gmail.com>
2021-08-04 12:15:47 +0530

    Queue

---
+++ Queue/traversal.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#include<queue>
+using namespace std;
+
+int main(){
+    queue <int> q;
+    q.push(10);
+    q.push(20);
+    q.push(30);
+    q.push(40);
+    q.push(50);
+    while (q.empty()==false)
+    {
+        cout<<q.front()<<" "<<q.back()<<endl;
+        q.pop();
+    }
+
+}
