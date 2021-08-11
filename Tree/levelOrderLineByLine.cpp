c4402865acd2ed6a4c1ca9ec77a9aa46555f2080
vidit <viditgaur1209@gmail.com>
2021-08-11 22:16:47 +0530

    Tree

---
+++ Tree/levelOrderLineByLine.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node{
+    int key;
+    node*right,*left;
+    node (int k){
+        key=k;
+        left=right=NULL;
+    }
+};
+
+void printLevel(node *root){
+    if(root ==NULL) return;
+
+    queue<node *>q;
+    q.push(root);
+
+    while(q.empty()==false){
+        int count=q.size();
+        for(int i=0;i<count;i++){
+            node *curr=q.front();
+            q.pop();
+            cout<<curr->key<<" ";
+
+            if(curr->left!=NULL) q.push(curr->left);
+            if(curr->right!=NULL) q.push(curr->right);
+        }
+        cout<<"\n";
+    }
+}
+int main(){
+    node *root=new node(10);
+    root->left=new node(20);
+    root->right=new node(30);
+    root->left->left=new node(40);
+    root->right->right=new node(50);
+    printLevel(root);
+}
